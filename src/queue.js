fb2176a19fa1abb35a87894cdf5b7a78054a13b8
martorija <martorija@mail.ru>
2022-10-17 23:59:42 +0300

    update queue.js

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+let stack =[];
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+//     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+//     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const enqueue = (value) => queue.push(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+//     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const dequeue=()=> queue.shift();
   }
 }
 
