ede0e75a5640cd4e0471312c2c75a836ae910736
martorija <martorija@mail.ru>
2022-10-18 00:02:06 +0300

    Update stack.js

--- src/stack.js
+++ src/stack.js
@@ -13,21 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  let stack =[];
+//   let stack =[];
   push(element) {
 //     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    this.stack.push(element);
+//     this.stack.push(element);
   }
 
   pop() {
 //     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    this.stack.pop();
+//     this.stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+//     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
