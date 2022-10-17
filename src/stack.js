4dde10a491f66f6ca7f2439bba384a315f765681
martorija <martorija@mail.ru>
2022-10-18 00:04:43 +0300

    Update stack.js

--- src/stack.js
+++ src/stack.js
@@ -13,7 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-   let stack =[];
+   let stack =new Stack();
   push(element) {
 //     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
